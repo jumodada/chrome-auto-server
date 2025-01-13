43a3f56f365652a6c160517372f5b1c3ae328b37
wukunyun <864353360@qq.com>
2025-01-13 16:10:12 +0800

    update demo

--- migrations.sh
+++ migrations.sh
@@ -1,5 +1,16 @@
+#!/bin/bash
 
 # 生成迁移文件
-alembic revision --autogenerate -m "Auto generate migration"
-
-alembic upgrade head
+if alembic revision; then
+    echo "成功生成迁移文件"
+    # 执行迁移
+    if alembic upgrade head; then
+        echo "成功执行迁移"
+    else
+        echo "执行迁移失败"
+        exit 1
+    fi
+else
+    echo "生成迁移文件失败"
+    exit 1
+fi
